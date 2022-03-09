a98d79e67694a138a276348cb02e2b06cf9c9506
CS416F19 <cs416f19@gmail.com>
2022-03-09 14:00:15 -0500

    Initial commit

---
+++ app/src/main/java/com/example/bottomnavigationexample/ui/settings/SettingsFragment.kt
@@ -0,0 +1,24 @@
+package com.example.bottomnavigationexample.ui.settings
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.bottomnavigationexample.R
+
+class SettingsFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = SettingsFragment()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Do something here
+        return inflater.inflate(R.layout.settings_fragment, container, false)
+    }
+
+}
